6ce854144999c7f26ca05fe049f0a241d6949ae3
mckim <kmc04@naver.com>
2021-08-29 01:12:11 +0900

    3. javascript - 돈의 흐름을 나타내기, 현재 자산 표시, 자산 요약을 나타내는 기능 추가

--- script.js
+++ script.js
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'k',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'J',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'S',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'M',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -36,61 +36,71 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
+const totalBalance = document.querySelector('.balance__total');
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+const summaryIn = document.querySelector('.income');
+const summaryOut = document.querySelector('.out');
+const summaryInterest = document.querySelector('.yield');
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+// functions
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+// 돈의 흐름을 나타냄
+const displayMovements = (movements) => {
+  movements.forEach((value,idx) => {
+    const type = value > 0 ? '입금' : '출금'
+    const typeClass = value > 0 ? 'deposit':'withdraw'
+    const html = `
+    <div class="movement__row">
+      <div class="movement__type__${typeClass}">${type} ${idx+1}</div>
+      <div class="movement__type__date">12/03/2020</div>
+      <div class="movement__type__value">₩${value}</div>
+    </div>
+    `;
 
+    containerMovements.insertAdjacentHTML('afterbegin',html);
+  })
+}
+displayMovements(account1.movements);
 
+// username 생성
+const createUsernames = (accs) => {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+    .toLowerCase()
+    .split(' ')
+    .map(name => name[0])
+    .join('')
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
 
-
-/////////////////////////////////////////////////
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-for(const movement of movements) {
-  if(movement > 0) {
-    console.log(`You deposited ${movement}`)
-  } else {
-    console.log(`You withdrew ${Math.abs(movement)}`)
-  }
+// 현재 자산을 나타냄
+const calcBalanace = (movements) => {
+  const balance = movements.reduce((prev,cur) => prev+cur);
+  totalBalance.innerHTML = `₩${balance}`;
 }
 
-movements.forEach((movement,a,array) => {
-  console.log(movement,a)
-  console.log(array)
-})
+calcBalanace(account1.movements);
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// 자산 요약을 나타냄
+const calcDisplaySummary = (movements) => {
+  const incomes = movements
+  .filter(mov => mov > 0)
+  .reduce((acum,mov) => acum+mov,0);
+  summaryIn.textContent = `₩${incomes}`;
 
-currencies.forEach((value,key,map) => {
-  console.log(value,key)
-})
+  const outcomes = movements
+  .filter(mov => mov < 0)
+  .reduce((acum,mov) => acum+mov,0);
+  summaryOut.textContent = `₩${Math.abs(outcomes)}`;
+
+  const interests = movements
+  .filter(mov => mov > 0)
+  .map(deposit => deposit * 1.2 / 100)
+  .reduce((acum,mov) => acum+mov,0);
+  summaryInterest.textContent = `₩${Math.floor(Math.abs(interests))}`;
+}
+
+calcDisplaySummary(account1.movements);
